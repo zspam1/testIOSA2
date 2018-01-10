a58f2dffdd728e5061124dd4f138581b056aad56
zspam1 <zspam1@gmail.com>
2018-01-09 21:09:25 -0800

    Add Forms

---
+++ testFormA1/testFormA1Page.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace testFormA1
+{
+    public partial class testFormA1Page : ContentPage
+    {
+        public testFormA1Page()
+        {
+            InitializeComponent();
+        }
+    }
+}
